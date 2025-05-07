9bf88d331bf098478e8b0573ee2aef324c91e1e6
Arthur01 <arthur.jerlstrom@gmail.com>
2025-05-08 00:00:36 +0200

    set up basic window and rectangle

--- main.cpp
+++ main.cpp
@@ -2,6 +2,34 @@
 
 int main()
 {
+    //create window
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Algorithm visualiser");
+
+    //create rectangle
+    sf::RectangleShape rectangle({400.f, 300.f});
+
+    //loop to keep open
+    while(window.isOpen()){
+
+        while (const std::optional event = window.pollEvent()){
+            if(event->is<sf::Event::Closed>()){
+                window.close();
+            }
+        }
+        //fill display with black
+        window.clear(sf::Color(150,150,150));
+
+        //draw rectangle
+        window.draw(rectangle);
+
+        //display what is set up
+        window.display();
+
+    }
+
+}
+
+/*
     sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
@@ -18,4 +46,4 @@ int main()
         window.draw(shape);
         window.display();
     }
-}
+*/
