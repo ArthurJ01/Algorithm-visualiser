65d625f87ab74e67f0e1147da6af49c4ddf61d6f
Arthur01 <arthur.jerlstrom@gmail.com>
2025-05-16 10:12:22 +0200

    added hold button and click

--- main.cpp
+++ main.cpp
@@ -9,6 +9,11 @@
 
 int main()
 {
+
+    //click logic
+    bool wasLeftPressed = false;
+    bool wasRightPressed = false;
+
     int arrSize = 35;
 
     std::vector<int> arr(arrSize);
@@ -21,8 +26,8 @@ int main()
     //create window, get size (size should probably be taken in main every frame)
     sf::RenderWindow window(sf::VideoMode({800, 600}), "Algorithm visualiser");
 
-    //insertionSort(arr, arrSize, steps);
-    mergeSort(arr, 0, arrSize - 1, steps);
+    insertionSort(arr, arrSize, steps);
+    //mergeSort(arr, 0, arrSize - 1, steps);
 
     long long unsigned step = 0;
     steps.push_back(arr);
@@ -43,17 +48,42 @@ int main()
 
         //display what is set up
         window.display();
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
-        if(sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)){
-            step++;
-            if(step > steps.size() - 1){
-                step = steps.size() - 1;
+        bool isLeftPressed = sf::Mouse::isButtonPressed(sf::Mouse::Button::Left);
+        if(isLeftPressed && !wasLeftPressed){
+            if(step < steps.size() - 1){
+                step++;
             }
 
         }
+        wasLeftPressed = isLeftPressed;
+
+        bool isRightPressed = sf::Mouse::isButtonPressed(sf::Mouse::Button::Right);
+        if(isRightPressed && !wasRightPressed){
+            if(step >= 1){
+                step--;
+            }
+
+        }
+        wasRightPressed = isRightPressed;
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+        {
+            if(step < steps.size() - 1){
+                step++;
+            }
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+        {
+            if(step >= 1){
+                step--;
+            }
+        }
+                
 
         currentStep = steps.at(step);
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
     }
 }
