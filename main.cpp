802b196c2fd524db3c8fb5f9514a79c0fae391b4
Arthur01 <arthur.jerlstrom@gmail.com>
2025-06-16 21:38:26 +0200

    fixed centering of buttons

--- main.cpp
+++ main.cpp
@@ -17,41 +17,7 @@ int main()
     SortingAlgorithm algoToRun = SortingAlgorithm::insertionSort;
     sf::RenderWindow window(sf::VideoMode({800, 600}), "Algorithm visualiser");
 
-    //TODO: import a font to the project or smth (maybe it can be like this though)
-    sf::Font font("C:/Windows/fonts/arial.ttf");
-
-    sf::Text text(font); // a font is required to make a text object
-    text.setString("menu ");
-    text.setCharacterSize(24); // in pixels, not points!
-    text.setFillColor(sf::Color::Red);
-    text.setStyle(sf::Text::Bold | sf::Text::Underlined);
-
-   sf::RectangleShape insertionSortButton({200.f, 100.f});
-   insertionSortButton.setPosition({100.f, 50.f}); 
-
-    sf::Text insertionButtonText (font); // a font is required to make a text object
-    insertionButtonText.setString("insertion sort ");
-    insertionButtonText.setCharacterSize(24);
-    insertionButtonText.setFillColor(sf::Color::Red);
-    {
-        float position_x = insertionSortButton.getPosition().x + (insertionSortButton.getSize().x / 2);
-        float position_y = insertionSortButton.getPosition().y + (insertionSortButton.getSize().y / 2);
-        insertionButtonText.setPosition({position_x, position_y});
-    }
-
-
-    sf::RectangleShape MergeSortButton({200.f, 100.f});
-    MergeSortButton.setPosition({100.f, 200.f}); 
-
-    sf::Text mergeButtonText (font); // a font is required to make a text object
-    mergeButtonText.setString("merge sort ");
-    mergeButtonText.setCharacterSize(24);
-    mergeButtonText.setFillColor(sf::Color::Red);
-    {
-        float position_x = MergeSortButton.getPosition().x + (MergeSortButton.getSize().x / 2);
-        float position_y = MergeSortButton.getPosition().y + (MergeSortButton.getSize().y / 2);
-        mergeButtonText.setPosition({position_x, position_y});
-    }
+    
 
     //loop to keep open
     while(window.isOpen()){
@@ -70,8 +36,45 @@ int main()
         switch (currentWindow){
             case Windows::menu: {
 
+                //TODO: import a font to the project or smth (maybe it can be like this though)
+                sf::Font font("C:/Windows/fonts/arial.ttf");
                 window.clear(sf::Color(100,100,100));
 
+                sf::Text text(font); // a font is required to make a text object
+                text.setString("menu ");
+                text.setCharacterSize(24); // in pixels, not points!
+                text.setFillColor(sf::Color::Red);
+                text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+
+                sf::RectangleShape insertionSortButton({200.f, 100.f});
+                insertionSortButton.setPosition({100.f, 50.f}); 
+
+                sf::Text insertionButtonText (font); // a font is required to make a text object
+                insertionButtonText.setString("insertion sort ");
+                insertionButtonText.setCharacterSize(24);
+                insertionButtonText.setFillColor(sf::Color::Red);
+                {
+                    float position_x = insertionSortButton.getPosition().x + (insertionSortButton.getSize().x / 2);
+                    float position_y = insertionSortButton.getPosition().y + (insertionSortButton.getSize().y / 2);
+                    insertionButtonText.setOrigin({insertionButtonText.getGlobalBounds().getCenter()});
+                    insertionButtonText.setPosition({position_x, position_y});
+                }
+
+
+                sf::RectangleShape MergeSortButton({200.f, 100.f});
+                MergeSortButton.setPosition({100.f, 200.f}); 
+
+                sf::Text mergeButtonText (font); // a font is required to make a text object
+                mergeButtonText.setString("merge sort ");
+                mergeButtonText.setCharacterSize(24);
+                mergeButtonText.setFillColor(sf::Color::Red);
+                {
+                    float position_x = MergeSortButton.getPosition().x + (MergeSortButton.getSize().x / 2);
+                    float position_y = MergeSortButton.getPosition().y + (MergeSortButton.getSize().y / 2);
+                    mergeButtonText.setOrigin({mergeButtonText.getGlobalBounds().getCenter()});
+                    mergeButtonText.setPosition({position_x, position_y});
+                }
+              
                 sf::Vector2f mouse_position = sf::Vector2f(sf::Mouse::getPosition(window));
 
                 if(insertionSortButton.getGlobalBounds().contains(mouse_position)){
