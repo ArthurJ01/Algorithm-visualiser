af320bd79be6b822beb9c1c237af208874e50653
Arthur01 <arthur.jerlstrom@gmail.com>
2025-06-18 14:30:49 +0200

    Added graph page

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 #include "headers/sortingVisualiser.hpp"
 #include "headers/button.hpp"
 
-enum class Windows {menu, sorting};
+enum class Windows {menu, sorting, graph};
 extern const sf::Font font("C:/Windows/fonts/arial.ttf");
 
 int main()
@@ -19,10 +19,10 @@ int main()
     SortingAlgorithm algoToRun = SortingAlgorithm::insertionSort;
     sf::RenderWindow window(sf::VideoMode({800, 600}), "Algorithm visualiser");
 
-    
     sf::Vector2f buttonSize = {200.f, 100.f};
     Button insertionSortButton(buttonSize, {100.f, 50.f}, "insertion sort");
-    Button MergeSortButton(buttonSize, {100.f, 200.f}, "merge sort");
+    Button mergeSortButton(buttonSize, {100.f, 200.f}, "merge sort");
+    Button graphButton(buttonSize, {400.f, 50.f}, "Graph builder");
 
     //loop to keep open
     while(window.isOpen()){
@@ -48,19 +48,48 @@ int main()
                     currentWindow = Windows::sorting;
                     algoToRun = SortingAlgorithm::insertionSort;
                 }
-
-                if(MergeSortButton.clicked(mouse_position)){
+                if(mergeSortButton.clicked(mouse_position)){
                     currentWindow = Windows::sorting;
                     algoToRun = SortingAlgorithm::mergeSort;
                 }
+                if(graphButton.clicked(mouse_position)){
+                    currentWindow = Windows::graph;
+                }
 
                 window.draw(insertionSortButton);
-                window.draw(MergeSortButton);
+                window.draw(mergeSortButton);
+                window.draw(graphButton);
                 window.draw(text);
                 window.display();
 
                 break;
             }
+
+            case Windows::graph:{
+
+                bool wantToExit = false;
+
+                while(!wantToExit && window.isOpen()){
+
+                    window.clear({0,0,0});
+
+                    while (const std::optional event = window.pollEvent()){
+                        if(event->is<sf::Event::Closed>()){
+                            window.close();
+                        }
+                        else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+                        {
+                            if (keyPressed->scancode == sf::Keyboard::Scancode::Escape){
+                                wantToExit = true;
+                            }
+                        }
+                    }
+                    window.display();
+                }
+                currentWindow = Windows::menu;
+                break;
+            }
+
             case Windows::sorting: {
                 //run sorting algo and when it is done, go back to menu
                 runSortAlgo(window, algoToRun);
